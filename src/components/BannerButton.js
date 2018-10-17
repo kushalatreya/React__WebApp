9bb8391a6f970ff53d55eb67d1381453ba9c8dcf
kushal atreya <kushalatreya@live.com>
2018-10-17 15:32:25 +0300

    making button as a component

---
+++ src/components/BannerButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BannerButton(){
+    return(
+         <button className="start-btn" autoFocus={true}>Get started</button>
+    );
+}
+
+export default BannerButton;
