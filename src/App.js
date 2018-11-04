0cd28011e2bd923a923d31da79fcf7f1fce7ebe4
kushal atreya <kushalatreya@live.com>
2018-11-04 22:58:35 +0200

    add responsiveness, refactor the code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import Main from './components/Main';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <Main/>
         <Footer/>
       </div>
     );
